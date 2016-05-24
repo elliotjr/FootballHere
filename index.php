3bf0f1c1389c9c8fd436a41d569457982af8fc25
Winston Fang <winstonfang6@yahoo.com.au>
2016-05-24 15:42:58 +1000

    - changed bgc - styled add game form - added user admin area at top right of page

--- index.php
+++ index.php
@@ -10,110 +10,144 @@
 </head>
 
 <body>
-  <form class="" action="logout.php" method="post">
-      <input class="logoutButton"type="submit" name="logout" value="Logout">
-  </form>
 
-
-  <?php
+    <?php
     session_start();
     if (!isset($_SESSION['username'])) {
-      header('location: login.php');
+        //The website shouldn't need a login immediately.
+//      header('location: login.php');
     }
    ?>
 
 
-    <noscript>
-        <h1>Oops!</h1>
-        <h2>This site needs JavaScript to work properly!</h2>
-        <p>Try turning JavaScript on, or use a different browser.</p>
-    </noscript>
+        <noscript>
+            <h1>Oops!</h1>
+            <h2>This site needs JavaScript to work properly!</h2>
+            <p>Try turning JavaScript on, or use a different browser.</p>
+        </noscript>
 
-    <div class="darkOverlay"></div>
-
-    <div class="loginOverlay">
-        <div id="loginForm">
-            <h1>Add a Game</h1>
-
-            <form class="addGame" action="addGameToDb.php" method="post">
-
-                <input type="text" name="state" value="" placeholder="State">
-                <input type="text" name="city" value="" placeholder="City">
-                <input type="text" name="suburb" value="" placeholder="Suburb">
-                <input type="text" name="street" value="" placeholder="Street">
-                <input type="text" name="number" value="" placeholder="Number">
-
-                <input type="date" name="date" value="">
-                <input type="text" name="kickoff" value="" placeholder="Kick-Off Time (24hr)">
-                <input type="text" name="playersneeded" value="" placeholder="Players Needed">
-                <select name="skill">
-                  <option value="1">Beginner</option>
-                  <option value="2">Amateur</option>
-                  <option value="3">Advanced</option>
-                </select>
-                <input class="save" type="button" value="Submit" />
+        <div id="adminArea">
+            <p>Welcome Username</p>
+            <form class="logoutForm" action="logout.php" method="post">
+                <input class="logoutButton" type="submit" name="logout" value="Logout">
             </form>
-
         </div>
 
-        <div id="signupForm">
-            <h1>Sign Up</h1>
-            <form action="signup.php" method="post">
-                <p>Email</p>
-                <input type="text" />
+        <div class="darkOverlay"></div>
 
-                <p>Password</p>
-                <input type="password" />
+        <div class="loginOverlay">
+            <div id="loginForm">
+                <h1>Add a Game</h1>
+                <form class="addGame" action="addGameToDb.php" method="post">
 
-                <input type="submit" value="Submit" />
-            </form>
+                    <div class="formGroup">
+                        <input type="text" name="state" required>
+                        <label>State</label>
+                    </div>
 
-            <div class="noAccount">
-                <p>Already have an account?</p>
-                <a id="goToLogin" href="#">Log In</a>
+                    <div class="formGroup">
+                        <input type="text" name="city" required>
+                        <label>City</label>
+                    </div>
+
+                    <div class="formGroup">
+                        <input type="text" name="suburb" required>
+                        <label>Suburb</label>
+                    </div>
+
+                    <div class="formGroup">
+                        <input type="text" name="street" required>
+                        <label>Street</label>
+                    </div>
+
+                    <div class="formGroup">
+                        <input type="text" name="number" required>
+                        <label>Number</label>
+                    </div>
+
+                    <div class="formGroup">
+                        <input type="text" onfocus="(this.type='date')" onfocusout="(this.type='text')" name="date" placeholder=" " required>
+                        <label>Date</label>
+                    </div>
+
+                    <!--                <input type="date" name="date" value="">-->
+
+                    <div class="formGroup">
+                        <input type="text" name="kickoff" required>
+                        <label>Kickoff Time (24hr)</label>
+                    </div>
+
+                    <div class="formGroup">
+                        <input type="text" name="playersneeded" required>
+                        <label>Players Needed</label>
+                    </div>
+
+
+
+                    <select name="skill">
+                        <option selected="true" disabled>Select Skill Level</option>
+                        <option value="1">Beginner</option>
+                        <option value="2">Amateur</option>
+                        <option value="3">Advanced</option>
+                    </select>
+                    <input class="save" type="button" value="Submit" />
+                </form>
+
+            </div>
+
+            <div id="signupForm">
+                <h1>Sign Up</h1>
+                <form action="signup.php" method="post">
+                    <p>Email</p>
+                    <input type="text" />
+
+                    <p>Password</p>
+                    <input type="password" />
+
+                    <input type="submit" value="Submit" />
+                </form>
+
+                <div class="noAccount">
+                    <p>Already have an account?</p>
+                    <a id="goToLogin" href="#">Log In</a>
+                </div>
             </div>
         </div>
-    </div>
 
-    <header>
-        <h1>football</h1>
-        <h1 class="bold">here</h1>
-    </header>
+        <header>
+            <h1>football</h1>
+            <h1 class="bold">here</h1>
+        </header>
 
-    <main>
-        <div id="toolbar">
-            <a id="loginButton" href="#">Add a Football Game</a>
-            <a class="filterButton whiteFilterButton" href="#">Filter Results</a>
-            <a class="games" href="#">Your Games</a>
-        </div>
+        <main>
+            <div id="toolbar">
+                <a id="loginButton" href="#">Add a Football Game</a>
+                <a class="filterButton whiteFilterButton" href="#">Filter Results</a>
+                <a class="games" href="#">Your Games</a>
+            </div>
 
-        <div id="filters">
+            <div id="filters">
 
-            <nav id="skillLevel">
-                <a href="#" value="1" class="skill">Beginner</a>
-                <a href="#" value="2" class="skill">Amateur</a>
-                <a href="#" value="3" class="skill">Advanced</a>
-            </nav>
-        </div>
+                <nav id="skillLevel">
+                    <a href="#" value="1" class="skill">Beginner</a>
+                    <a href="#" value="2" class="skill">Amateur</a>
+                    <a href="#" value="3" class="skill">Advanced</a>
+                </nav>
+            </div>
 
-        <div id="yourGames">
-          <?php include('yourGames.php') ?>
-        </div>
+            <div id="yourGames">
+                <?php include('yourGames.php') ?>
+            </div>
 
-        <div id="map"></div>
-    </main>
-
-
-    <footer>
-        <p>designed by <a target="_blank" href="http://www.winstonfang.com">winston fang</a> 2016</p>
-    </footer>
+            <div id="map"></div>
+        </main>
 
 
-    <script type="text/javascript" src="./js/maps.js"></script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAKnGYIJ_jRTGNnNo0s3EgC90PnZxIW5Bg&libraries=places&callback=initMap" async defer></script>
-    <script type="text/javascript" src="./js/login.js"></script>
-    <script type="text/javascript" src="./js/toolbar.js"></script>
-    <script type="text/javascript" src="ajax.js"></script>
+        <script type="text/javascript" src="./js/maps.js"></script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAKnGYIJ_jRTGNnNo0s3EgC90PnZxIW5Bg&libraries=places&callback=initMap" async defer></script>
+        <script type="text/javascript" src="./js/login.js"></script>
+        <script type="text/javascript" src="./js/toolbar.js"></script>
+        <script type="text/javascript" src="ajax.js"></script>
 
 
 </body>
